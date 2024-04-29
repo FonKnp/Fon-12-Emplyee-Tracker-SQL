74a9a0907fc2b7ac6ec345f116eddcec65d3df29
FonKnp <fonnfonk@gmail.com>
2024-04-29 11:44:30 -0400

    revisit view all with try catch

--- index.js
+++ index.js
@@ -1,67 +1,67 @@
 const mysql = require('mysql2/promise');
 const { prompt } = require('inquirer');
+
 // connect to databases
 let db;
-
 (async () => {
-    try {
-        db = await mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'Dumpling6116',
-            database: 'employee_tracker',
-        });
-        console.log('Connected to the Employee Manager database.');
-        startApp();
-    } catch (err) {
-        console.error('Error connecting to the database:', err);
+  try {
+    db = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'Dumpling6116',
+      database: 'employee_tracker',
+    });
+    console.log('Connected to the Employee Manager database.');
+    startApp();
+  } catch (err) {
+    console.error('Error connecting to the database:', err);
     }
 })();
 
 // initial app start
 async function startApp() {
   try {
-      const response = await prompt({
-          type: 'list',
-          name: 'options',
-          message: 'What would you like to do? Choose below',
-          choices: [
-              'View All Departments',
-              'Add Department',
-              'View All Roles',
-              'Add Role',
-              'View All Employees',
-              'Add Employee',
-              'Update Employee Role',
-              'Exit'
-          ],
-      });
+    const response = await prompt({
+      type: 'list',
+      name: 'options',
+      message: 'What would you like to do? Choose below',
+      choices: [
+        'View All Departments',
+        'Add Department',
+        'View All Roles',
+        'Add Role',
+        'View All Employees',
+        'Add Employee',
+        'Update Employee Role',
+        'Exit'
+      ],
+    });
 
-      switch (response.options) {
-          case 'View All Departments':
-              await allDepartment();
-              break;
-          case 'Add Department':
-              await addDepartment();
-              break;
-          case 'View All Roles':
-              await allRoles();
-              break;
-          case 'Add Role':
-              await addRole();
-              break;
-          case 'View All Employees':
-              await allEmployees();
-              break;
-          case 'Add Employee':
-              await addEmployee();
-              break;
-          case 'Update Employee Role':
-              await updateRole();
-              break;
-          case 'Exit':
-              console.log('Exited successfully! Goodbye!');
-              process.exit();
+    switch (response.options) {
+      case 'View All Departments':
+        await allDepartment();
+        break;
+      case 'Add Department':
+        await addDepartment();
+        break;
+      case 'View All Roles':
+        await allRoles();
+        break;
+      case 'Add Role':
+        await addRole();
+        break;
+      case 'View All Employees':
+        await allEmployees();
+        break;
+      case 'Add Employee':
+        await addEmployee();
+        break;
+      case 'Update Employee Role':
+        await updateRole();
+        break;
+      case 'Exit':
+        console.log('Exited successfully! Goodbye!');
+        process.exit();
       }
   } catch (error) {
       console.error('Error:', error);
@@ -70,44 +70,94 @@ async function startApp() {
 
 // view all table from departments
 async function allDepartment() {
-  const [rows] = await db.query('SELECT * FROM departments');
-  console.table(rows);
+  try {
+    const [rows] = await db.query('SELECT * FROM departments');
+    console.table(rows);
+  } catch (err) { 
+    console.error(err); 
+  }
   startApp();
 }
 
 // view all roles
 async function allRoles() {
-  const [rows] = await db.query('SELECT roles.title, roles.id, departments.department_name, roles.salary FROM roles JOIN departments ON roles.department_id = departments.id');
-  console.table(rows);
+  try {
+    const [rows] = await db.query('SELECT roles.title, roles.id, departments.department_name, roles.salary FROM roles JOIN departments ON roles.department_id = departments.id');
+    console.table(rows);
+  } catch (err) { 
+    console.error(err); 
+  }
   startApp();
 }
 
 
 // view all employees
 async function allEmployees() {
-  const query = `
-      SELECT e.id, e.first_name, e.last_name, r.title, d.department_name, r.salary, CONCAT(m.first_name, " ", m.last_name) AS manager_name
-      FROM employee e
-      LEFT JOIN roles r ON e.role_id = r.id
-      LEFT JOIN departments d ON r.department_id = d.id
-      LEFT JOIN employee m ON e.manager_id = m.id;
-      `;
-  const [rows] = await db.query(query);
-  console.table(rows);
+  try {
+    const query = `
+    SELECT e.id, e.first_name, e.last_name, r.title, d.department_name, r.salary, CONCAT(m.first_name, " ", m.last_name) AS manager_name
+    FROM employee e
+    LEFT JOIN roles r ON e.role_id = r.id
+    LEFT JOIN departments d ON r.department_id = d.id
+    LEFT JOIN employee m ON e.manager_id = m.id;
+    `;
+    const [rows] = await db.query(query);
+    console.table(rows);
+  } catch (err) { 
+    console.error(err); 
+  }
   startApp();
 };
 
 //add department
 async function addDepartment() {
-  const response = await prompt({
+  try {
+    const response = await prompt({
       type: 'input',
       name: 'name',
       message: 'What new department you like to create?'
-  });
-
-  await db.query('INSERT INTO departments (department_name) VALUES (?)', [response.name]);
-  console.log(`You have successfully added department ${response.name} to the database!`);
-  startApp();
+    });
+    await db.query('INSERT INTO departments (department_name) VALUES (?)', [response.name]);
+    console.log(`You have successfully added department ${response.name} to the database!`);
+    startApp();
+  } catch (err) { 
+    console.error(err);
+    startApp();
+  }
 }
 
-startApp();
+//add roles
+async function addRole() {
+  try {
+    const [rows] = await db.query('SELECT id AS value, department_name AS name FROM departments');
+    const choices = rows.map(row => ({ name: row.name, value: row.value }));
+    
+    const response = await prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What role would you like to create?',
+      },
+      {
+        type: 'input',
+        name: 'salary',
+        message: 'Enter salary of the role you created:',
+      },
+      {
+        type: 'list',
+        name: 'department',
+        message: 'Which department this role will be in?',
+        choices: choices,
+      },
+    ]);
+    
+    const department = rows.find(row => row.value === response.department);
+    await db.query('INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)', [response.title, response.salary, response.department]);
+    
+    console.log(`You have successfully added role ${response.title} to the ${department.name} department with salary ${response.salary} to the database!`);
+  } catch (err) {
+    console.error(err);
+  }
+  startApp();
+};
+  
